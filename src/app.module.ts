aa214e56df8c23bd1613b89234bbbfd8d30176c4
Shaikh Al Amin <amin@liberate-labs.com>
2024-11-26 23:08:02 +0600

    fixing database connection issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configValidationSchema } from './common/schema/config-schema';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { getDatabaseConfig } from 'db/data-source';
+// import { getDatabaseConfig } from 'db/data-source';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from './common/exception/exception-filter';
 import { HttpModule } from '@nestjs/axios';
@@ -13,9 +13,23 @@ import { redisStore } from 'cache-manager-redis-yet';
 import { UserModule } from './modules/v1/user/user.module';
 import { AuthModule } from './modules/v1/auth/auth.module';
 import { StorageFilesModule } from './modules/v1/storage-files/storage-files.module';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 // import { ServeStaticModule } from '@nestjs/serve-static';
 // import { join } from 'path';
-
+import { DataSourceOptions } from 'typeorm';
+export const getDatabaseConfig = (
+  configService: ConfigService,
+): DataSourceOptions => ({
+  type: 'postgres',
+  useUTC: true,
+  url: configService.get<string>('DATABASE_URL'),
+  logging: configService.get<string>('NODE_ENV') == 'development',
+  synchronize: configService.get<string>('NODE_ENV') == 'development',
+  // entities: [`dist/**/entities/*.{ts,js}`],
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`dist/db/migrations/*.js`],
+  namingStrategy: new SnakeNamingStrategy(),
+});
 @Module({
   imports: [
     ConfigModule.forRoot({
